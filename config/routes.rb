cf4788e5de00e0e2dcdaeac0d768003c6276a638
Tasha Hutnick <ahutnick@sas.upenn.edu>
2020-09-30 18:20:41 -0400

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home', as: 'home'
   get '/about', to: 'static_pages#about', as: 'about'
   get '/sign-up', to: 'users#new', as: 'sign-up'
+
+  resources :users, except: [:index] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
