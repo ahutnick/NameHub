e7a986b3809e918f9458e915421698486cd94de4
Tasha Hutnick <ahutnick@sas.upenn.edu>
2020-09-30 15:21:53 -0400

    add users / signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home', as: 'home'
   get '/about', to: 'static_pages#about', as: 'about'
+  get '/sign-up', to: 'users#new', as: 'sign-up'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
