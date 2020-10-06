992c85a611a10ad51171cd66f602e67fa09ee5b8
Tasha Hutnick <ahutnick@sas.upenn.edu>
2020-10-06 15:00:41 -0400

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home', as: 'home'
   get '/about', to: 'static_pages#about', as: 'about'
-  get '/sign-up', to: 'users#new', as: 'sign-up'
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create', as: 'new_session'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users, except: [:index] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
