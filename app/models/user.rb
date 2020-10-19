d5912e5435009f09cb2299d19696e22278f3f764
Tasha Hutnick <ahutnick@sas.upenn.edu>
2020-10-19 13:53:31 -0400

    add project model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
+    has_many :projects
+
+    has_secure_password
+    
     attr_accessor :remember_token
-    before_validation { self.email.downcase! }
-    validates :name,  presence: true, length: { maximum: 50 }
+
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    validates :name,  presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
-    has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+    before_validation { self.email.downcase! }  
   
     class << self
         def digest(string)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_200927) do
+ActiveRecord::Schema.define(version: 2020_10_19_172707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_10_07_200927) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
