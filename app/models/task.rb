f0b518d71c53bbb8ac57982f69c251783f337ba1
Tasha Hutnick <ahutnick@sas.upenn.edu>
2020-10-21 20:55:06 -0400

    add new tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :project
 
+  STAGES = ['idea', 'ready', 'in progress', 'review', 'completed'].freeze
+
   validates :title, presence: true
+  validates :description, presence: true
+  validates :stage, presence: true, inclusion: { in: STAGES }
+
 end
