5d243ac2e69e02bea053bb0112bc9b5ccebcf2b5
Tasha Hutnick <ahutnick@sas.upenn.edu>
2020-10-24 15:44:32 -0400

    add task / project helpers

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,13 @@
 module ProjectsHelper
+    def progress
+        "#{completed} / #{total} Completed ( #{percent}% )"
+    end
+
+    def percent
+        ((completed / total.to_f) * 100).floor
+    end
+
+    def completed?
+        percent == 100
+    end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,9 @@
 module TasksHelper
+    def completed
+        Task.where(stage: "completed").count
+    end
+
+    def total
+        Task.count
+    end
 end
