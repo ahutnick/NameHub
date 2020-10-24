c3043a4787800470b7d4ad547874395c9d5ef08e
Tasha Hutnick <ahutnick@sas.upenn.edu>
2020-10-24 15:46:25 -0400

    change in progress to underway

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
 
-  STAGES = ['idea', 'ready', 'in progress', 'review', 'completed'].freeze
+  STAGES = ['idea', 'ready', 'underway', 'review', 'completed'].freeze
 
   validates :title, presence: true
   validates :stage, presence: true, inclusion: { in: STAGES }
